69a37bf67de5cfca2452033bb13c6dcc5b1cd185
kel8458 <pjfry651@gmail.com>
2019-07-09 10:18:15 +1000

    Fix to custom items and pearl drop rates

--- data/sql/custom/db_world/custom-pearl-increase.sql
+++ data/sql/custom/db_world/custom-pearl-increase.sql
@@ -1,3 +1,4 @@
 USE `world`;
 UPDATE `spell_loot_template` SET `Chance` = 7 WHERE `entry` = 58165 AND `item` = 7971;
 UPDATE `spell_loot_template` SET `Chance` = 7 WHERE `entry` = 58165 AND `item` = 13926;
+UPDATE `spell_loot_template` SET `Chance` = 33 WHERE `entry` = 58165 AND `item` = 7974;
